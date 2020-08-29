72f5fced16c1edbfd88492363ecfd917a49edef8
Pragun Bajracharya <pragunbaj99@gmail.com>
2020-08-29 18:52:23 +0545

    Eject react and changed css module

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch} from "react-router-dom";
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Switch>
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
-                </Switch>
-            </Router>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <BurgerBuilder/>
+                </Layout>
+            </div>
+        )
+    }
 }
 
 export default App;
